a810f8f79145ce19ac86cca42bda87c2c8b758a9
weibin268 <binweizhuang@hotmail.com>
2019-06-05 21:41:20 +0800

    a

---
+++ zwb_utils/date.py
@@ -0,0 +1,17 @@
+import datetime
+import time
+
+DEFUALT_DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+
+def to_string(date):
+    return date.strftime(DEFUALT_DATETIME_FORMAT)
+
+
+def to_datetime(str):
+    return datetime.datetime.strptime(str, DEFUALT_DATETIME_FORMAT)
+
+
+if __name__ == "__main__":
+    print(to_string(datetime.datetime.now()))
+    print(to_datetime("2019-01-01 00:00:00"))
